412955c5ec1f98c4824f1b8158a00844244d17e3
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-06-08 19:08:20 +0600

    Fix issues

--- src/app/jobs/[id]/page.jsx
+++ src/app/jobs/[id]/page.jsx
@@ -1,3 +1,4 @@
+
 import { getJobById } from '@/lib/api/jobs';
 import React from 'react';
 
@@ -8,16 +9,16 @@ const page = async({params}) => {
     console.log(job);
 
     // Guard clause in case API fails or returns null
-    // if (!job) {
-    //     return (
-    //         <div className="w-full min-h-screen bg-zinc-950 flex flex-col justify-center items-center text-white p-6">
-    //             <p className="text-zinc-400 text-lg">Job position could not be found or is no longer active.</p>
-    //         </div>
-    //     );
-    // }
+    if (!job) {
+        return (
+            <div className="w-full min-h-screen bg-zinc-950 flex flex-col justify-center items-center text-white p-6">
+                <p className="text-zinc-400 text-lg">Job position could not be found or is no longer active.</p>
+            </div>
+        );
+    }
     return (
         <div>
-            <h2>Job details page dose not work</h2>
+            <h2>Job details page dose not work is now fixed</h2>
         </div>
     );
 };
