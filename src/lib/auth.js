3aa1d8e7597b6f9144b7b17ec8f4eeae2a62d7cb
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-06-17 00:03:52 +0600

    Added more things

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -4,6 +4,7 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { admin } from "better-auth/plugins";
 
 const client = new MongoClient(process.env.MONGO_DB_URI);
 const db = client.db(process.env.AUTH_DB_NAME);
@@ -26,5 +27,8 @@ export const auth = betterAuth({
                 default: 'seeker_free'
             }
         }
-    }
+    },
+    plugins: [
+        admin()
+    ]
 });
--- src/lib/core/session.js
+++ src/lib/core/session.js
@@ -11,6 +11,15 @@ export const getUserSession = async () => {
 };
 
 
+export const getUserToken = async () => {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
+    return session?.session?.token || null;
+}
+
+
 export const requireRole = async (role) => {
     const user = await getUserSession()
     if (!user) {
